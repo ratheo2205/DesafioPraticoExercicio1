76322d1c1ee1532eefdaef9f918799987059fbea
ratheo2205 <rafaelgs84@gmail.com>
2024-10-31 22:04:41 -0300

    # Add Console.ReadLine to Program,cs

--- DesafioPraticoExercicio1/Program.cs
+++ DesafioPraticoExercicio1/Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+Console.WriteLine("Informe o seu nome: ");
+var nome = Console.ReadLine();
+Console.WriteLine("Olá "+nome+"! Seja bem vindo!");
+
